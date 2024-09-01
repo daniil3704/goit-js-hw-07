9903273a74a53e7cbe8d51542ab3349de916f3d4
Даниил Кузнецов <daniil190175@gmail.com>
2024-09-01 14:56:58 +0300

    fixes

--- js/task-6.js
+++ js/task-6.js
@@ -4,48 +4,48 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const dataCreate = document.querySelector('[data-create]')
-const dataDestroy = document.querySelector('[data-destroy]')
-const divBoxes = document.getElementById('boxes')
-const inputField = document.querySelector('input')
-let inputValue = 0
-
+const dataCreate = document.querySelector("[data-create]");
+const dataDestroy = document.querySelector("[data-destroy]");
+const divBoxes = document.getElementById("boxes");
+const inputField = document.querySelector("input");
+let inputValue = 0;
 
 function createBoxes(amount) {
-  let divArray = []
-  let counter = 0
-  let startDiv = 30
+  destroyBoxes()
+  let divArray = [];
+  let counter = 0;
+  let startDiv = 30;
   for (let i = 0; i < amount; i++) {
-    divArray[counter] = document.createElement('div')
-    divArray[counter].style.width = `${startDiv}px`;           
-    divArray[counter].style.height = `${startDiv}px`;         
+    divArray[counter] = document.createElement("div");
+    divArray[counter].style.width = `${startDiv}px`;
+    divArray[counter].style.height = `${startDiv}px`;
     divArray[counter].style.backgroundColor = getRandomHexColor();
-    startDiv+= 10
-    counter++
+    startDiv += 10;
+    counter++;
   }
-  divBoxes.append(...divArray)
+  divBoxes.append(...divArray);
+  inputField.value = ''
 }
 
-function destroyBoxes(){
-   const divArray = divBoxes.querySelectorAll('div')
-   divArray.forEach(div => {
-    div.remove()
-   });
+function destroyBoxes() {
+  const divArray = divBoxes.querySelectorAll("div");
+  divArray.forEach((div) => {
+    div.remove();
+    inputField.value = ''
+  });
 }
 
 inputField.addEventListener("input", () => {
   inputValue = inputField.value;
-})
+});
 
 dataCreate.addEventListener("click", () => {
-  if (inputValue >=1 && inputValue <= 100){
-     createBoxes(inputValue)
-   }
-   else{
-    
-   } 
-})
+  if (inputValue >= 1 && inputValue <= 100) {
+    createBoxes(inputValue);
+  } else {
+  }
+});
 
 dataDestroy.addEventListener("click", () => {
-  destroyBoxes()
-})
+  destroyBoxes();
+});
